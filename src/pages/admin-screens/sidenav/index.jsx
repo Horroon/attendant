61c5872924a795f361a6540100efe247b548ab5b
Horroon <horroona@gmail.com>
2021-04-19 23:06:35 +0500

    admin first view ready

--- src/pages/admin-screens/sidenav/index.jsx
+++ src/pages/admin-screens/sidenav/index.jsx
@@ -17,23 +17,37 @@ export const AdminSideNav = () => {
       <div className={styles.adminroles}>
         <div className={styles.roleone}>
           <div className={styles.today}>
-            <i class="fa fa-fw fa-home"></i> Today Availability
+            <i className="fas fa-list"></i> <span>Today Availability</span>
           </div>
           <div className={styles.nestedrole}>
-              <ul>
-                  <li>Available</li>
-                  <li>Unavailble</li>
-                  <li>On Leaves</li>
-              </ul>
+            <ul>
+              <li>
+                {" "}
+                <i className="fa fa-clock" /> Available
+              </li>
+              <li>
+                {" "}
+                <i className="fas fa-pencil-alt" /> Unavailble
+              </li>
+              <li>
+                {" "}
+                <i className="fas fa-sign-out-alt" /> On Leaves
+              </li>
+            </ul>
           </div>
         </div>
         <div>
-        <div className={styles.roleone}>
-          <div className={styles.today}>
-            <i class="fa fa-fw fa-home"></i> Overall Stats
+          <div className={styles.roletwo}>
+            <div className={styles.ostate}>
+              <i class="fas fa-clipboard"></i> Overall Stats
+            </div>
           </div>
         </div>
-        </div>
+      </div>
+      <div>
+        <a href="#" className="float">
+          <i class="fas fa-cog"></i>
+        </a>
       </div>
     </div>
   );
